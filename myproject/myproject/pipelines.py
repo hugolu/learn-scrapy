8a835aabea9253114f27f2e261cfea4e8413ef74
Hugo Lu <printk@gmail.com>
2017-02-01 11:27:26 +0800

    update meizitu

--- myproject/myproject/pipelines.py
+++ myproject/myproject/pipelines.py
@@ -4,7 +4,9 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+import os
+import requests
+from myproject import settings
 
 class MyprojectPipeline(object):
     def process_item(self, item, spider):
@@ -13,6 +15,37 @@ class MyprojectPipeline(object):
 class ImageDownloadPipeline(object):
     def process_item(self, item, spider):
         if 'img_urls' in item:
+            dirpath = '%s/%s' % (settings.IMAGES_STORE, spider.name)
+            if not os.path.exists(dirpath):
+                os.makedirs(dirpath)
+
+            request_data = {'allow_redirects': False,
+             'auth': None,
+             'cert': None,
+             'data': {},
+             'files': {},
+             'headers': {'User-Agent': settings.USER_AGENT},
+             'method': 'get',
+             'params': {},
+             'proxies': {},
+             'stream': True,
+             'timeout': 30,
+             'url': '',
+             'verify': True}
+
             for url in item['img_urls']:
                 print('    >> %s' % url)
+
+                filename = '_'.join(url.split('/')[5:])
+                filepath = '%s/%s' % (dirpath, filename)
+                if os.path.exists(filepath):
+                    continue
+
+                request_data['url'] = url
+                with open(filepath, 'wb') as handle:
+                    response = requests.request(**request_data)
+                    for block in response.iter_content(8192):
+                        if not block:
+                            break
+                        handle.write(block)
         pass
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -89,3 +89,5 @@ IMAGES_STORE = 'images'
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36'
